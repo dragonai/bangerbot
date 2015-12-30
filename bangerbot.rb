77399c02349d0cffd6cb51abc178695ebf41fa97
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2015-12-30 05:43:38 -0500

    cleanup

--- bangerbot.rb
+++ bangerbot.rb
@@ -15,11 +15,16 @@ scheduler.every '10d', :first_in => '0.1s' do
 		:restrict_to_subreddit => true,
 		:limit => 50
 		})
-	banger_array = top_bangers.results.map { |banger| banger.url.sub('https://', 'http://') }
+	banger_array = top_bangers.results.map { |banger| banger.url }
 
 end
 
 get '/' do
 	content_type :json
-	{ :response_type => "in_channel", :text => banger_array.sample, :unfurl_media => true, :unfurl_text => true }.to_json
+	{
+		:response_type => "in_channel",
+		:text => banger_array.sample,
+		:unfurl_media => true,
+		:unfurl_text => true
+	}.to_json
 end
